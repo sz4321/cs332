1e5acdef29693e5659617eca1ce7b5e8a82587b3
nrs32 <nrs32@gold12.cs.calvin.edu>
2019-11-19 21:07:14 -0500

    impliment file transport between file and server

--- RCMP/talk_client_skeleton.py
+++ RCMP/talk_client_skeleton.py
@@ -1,6 +1,6 @@
-# Client to handle I/O messages on client end
+# Client to send file to another client user server
 # Edited by: Nikita Sietsema and Sebrina Zeleke
-# Date: 10.22.19
+# Date: 11.19.19
 
 import select
 import socket
@@ -11,69 +11,104 @@ import os, errno
 # set up commands for user
 parser = argparse.ArgumentParser(description="A prattle client")
 
-parser.add_argument("-n", "--name", dest="name", help="name to be prepended in messages (default: machine name)")
 parser.add_argument("-s", "--server", dest="server", default="127.0.0.1",
                     help="server hostname or IP address (default: 127.0.0.1)")
 parser.add_argument("-p", "--port", dest="port", type=int, default=12345,
-                    help="TCP port the server is listening on (default 12345)")
+                    help="UDP port the server is listening on (default 12345)")
+parser.add_argument("-f", "--filename", dest="filename", default='text.txt',
+                    help="write filename include extension to send to other client")
+
+# Optional verbose for more info
 parser.add_argument("-v", "--verbose", action="store_true", dest="verbose",
                     help="turn verbose output on")
 args = parser.parse_args()
+
+
+####################################  Main Logic ####################################
+PACKET_SIZE = 10
+
+if not args.verbose:
+    print('Use -v to turn on verbose')
 
 try:
     # Create socket
     clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     if args.verbose:
-        print '\tverbose: created clientSocket'
+        print('Created clientSocket')
 
     # Connect to server
     clientSocket.connect((args.server, args.port))
     if args.verbose:
-        print '\tverbose: connected to server on port', args.port
+        print('Connected to server on port', args.port)
 
-    print "Houston we are live!"
+    # Open file to be sent
+    filename=args.filename
+    fileSrc = open(filename,'rb')
 
-    # Set up infinite loop to listen for messages and talk
-    while True: 
-        try:
-            # Make input source be clientSocket
-            inputs, outputs, errors = select.select([clientSocket, sys.stdin], [sys.stdout], [])
+    # Read data into packets
+    data = fileSrc.read(PACKET_SIZE)
+    while (data):
+        # Send packet to server
+        clientSocket.send(data)
+        dataSize = len(data)
 
-            for i in inputs:
-                # Handle keyboard input
-                if i == sys.stdin:
-                    if args.verbose:
-                        print '\tverbose: Just recieved input from keyboard and sent a message'
-                    message = args.name + "> " + sys.stdin.readline()
-                    clientSocket.send(message.encode())
-                    print ''
-
-                # Handle message recieved
-                elif i == clientSocket:
-                    if args.verbose:
-                        print '\tverbose: Just recieved a message from socket'
-                    text = clientSocket.recv(100)
-                    if text == "":
-                        raise RuntimeError
-                    print text
-
-        # Handle errors gracefully
-        except KeyboardInterrupt:
-            print "Thanks for chatting, goodbye"
-            clientSocket.close()
+        if args.verbose:
+            print('Data: ', repr(data), "\nSize: ", dataSize)
+        
+        # If packet is less than PACKET_SIZE then 
+        # close file and break sending loop
+        if dataSize < PACKET_SIZE:
+            fileSrc.close()
             break
-        except RuntimeError:
-            print "Houston we have a problem: \n\tLost connection to server, goodbye"
-            clientSocket.close()
-            break
-        except errno:
-            print os.strerror(errno.errocode)
-            clientSocket.close()
-            break
-        except:
-            print "Unknown error"
-            clientSocket.close()
-            break
+
+        # Get next packet
+        data = fileSrc.read(PACKET_SIZE)
+
+    print('Done sending')
+    clientSocket.close()
+    
+
+#     # Set up infinite loop to listen for messages and talk
+#     while True: 
+#         try:
+#             # Make input source be clientSocket
+#             inputs, outputs, errors = select.select([clientSocket, sys.stdin], [sys.stdout], [])
+
+#             for i in inputs:
+#                 # Handle keyboard input
+#                 if i == sys.stdin:
+#                     if args.verbose:
+#                         print('\tverbose: Just recieved input from keyboard and sent a message')
+#                     message = args.name + "> " + sys.stdin.readline()
+#                     clientSocket.send(message.encode())
+#                     print('')
+
+#                 # Handle message recieved
+#                 elif i == clientSocket:
+#                     if args.verbose:
+#                         print('\tverbose: Just recieved a message from socket')
+#                     text = clientSocket.recv(100)
+#                     if text == "":
+#                         raise RuntimeError
+#                     print(text)
+
+#         # Handle errors gracefully
+#         except KeyboardInterrupt:
+#             print("Thanks for chatting, goodbye")
+#             clientSocket.close()
+#             break
+#         except RuntimeError:
+#             print("Houston we have a problem: \n\tLost connection to server, goodbye")
+#             clientSocket.close()
+#             break
+#         except errno:
+#             print(os.strerror(errno.errocode))
+#             clientSocket.close()
+#             break
+#         except:
+#             print("Unknown error")
+#             clientSocket.close()
+#             break
 except:
-    print "Could not connect to server."
+    print("Could not connect to server.")
     clientSocket.close()
--- RCMP/talk_server_skeleton.py
+++ RCMP/talk_server_skeleton.py
@@ -1,14 +1,68 @@
-# Server skeleton to handle I/O messages on server end
-# Author: Prof Norman
+# Server to recieve files and write to given destination file
+# Editors: Sebrina and Nikita
 
 import socket
 import select
 import argparse
+import sys
 
 parser = argparse.ArgumentParser(description="A prattle server")
 
 parser.add_argument("-p", "--port", dest="port", type=int, default=12345,
-                    help="TCP port the server is listening on (default 12345)")
+                    help="UDP port the server is listening on (default 12345)")
+parser.add_argument("-f", "--fileDest", dest="fileDest", default='result.txt',
+                    help="filename include extension to write results to")
+
+# optional for feedback
 parser.add_argument("-v", "--verbose", action="store_true", dest="verbose",
                     help="turn verbose output on")
+
+# optional specified host
+parser.add_argument("-host", "--hostname", dest="hostname", default="127.0.0.1",
+                    help="server hostname or IP address (default: 127.0.0.1)")
+
 args = parser.parse_args()
+
+####################################  Main Logic ####################################
+# Store variables
+PACKET_SIZE = 10
+port = args.port
+serverSocket = socket.socket()
+host = args.hostname
+
+# Start server listening
+serverSocket.bind((host, port))
+serverSocket.listen(5)
+print('Server listening....')
+if not args.verbose:
+    print('Use -v to turn on verbose')
+
+# Accept a client connection
+conn, addr = serverSocket.accept()
+if args.verbose:
+    print('Got connection from', addr)
+
+# Open file to write to
+with open(args.fileDest, 'wb') as f:
+    while True:
+        # Recieve data
+        if args.verbose: 
+            print('receiving data...')
+        data = conn.recv(PACKET_SIZE)
+        dataSize = len(data)
+
+        # Print info if verbose is true
+        if args.verbose:
+            print('Data: ', (data), "\nSize: ", dataSize)
+
+        # write data to a file
+        f.write(data)
+
+        # End connection when packet is smaller 
+        # than packet size
+        if dataSize < PACKET_SIZE:
+            break
+
+f.close()
+conn.close()
+print('Connection closed, wrote file')
